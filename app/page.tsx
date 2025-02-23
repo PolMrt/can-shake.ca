0f5c916215253a583dd40d2bf6745cd48fcbee12
Pol Marnette <pol@hey.com>
2025-02-23 13:56:16 -0800

    [+] Add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { MapTable } from "@/components/MapTable/MapTable";
 import { useParseFile } from "@/hooks/useParseFile";
 
+export const metadata = {
+  title: "Earthquakes in Canada",
+  description: "History of earthquakes in Canada - can-shake.ca",
+};
+
 export default function Home() {
   return <MapTable />;
 }
